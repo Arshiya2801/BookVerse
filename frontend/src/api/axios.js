a9c9fe0cb797b2902fc10a2a6289d07a3ee0cece
Arshiya Singh <singharshiya28@gmail.com>
2025-10-05 11:26:44 +0530

    debug

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://bookverse-9r9b.onrender.com/api', 
+  baseURL: 'https://bookverse-9r9b.onrender.com', 
   timeout: 10000,
 });
 
